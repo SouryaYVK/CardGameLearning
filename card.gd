ed5a48596b5638993c0d3d61be367d3c38b58f62
SouryaYVK <sourya.y.v.k@gmail.com>
2026-04-14 17:37:41 +0530

    Two Card Snap Done. Bug: Both cards snap simultaneously on drop

--- card.gd
+++ card.gd
@@ -2,11 +2,14 @@ extends Area2D
 
 var dragging = false
 var drag_offset = Vector2.ZERO
+var partner_card = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Connect to the Area2D signals to themselves
-	pass # Replace with function body.
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+	# pass # Replace with function body.
 
 func _input_event(_viewport, event: InputEvent, _shape_idx) -> void:
 	# Detects the initial touch on Mobile Screen or the Left Mouse click
@@ -21,8 +24,26 @@ func _input_event(_viewport, event: InputEvent, _shape_idx) -> void:
 			z_index = 0
 			
 			# ToDo: Stacking Check triggers
+			_handle_drop()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if dragging:
 		global_position = get_global_mouse_position() - drag_offset
+
+func _on_area_entered(other_area):
+	partner_card = other_area
+
+		
+func _on_area_exited(other_area):
+	partner_card = null
+
+func _handle_drop():
+	# if not overlapping_cards.is_empty():
+		if partner_card != null:
+			var card_width = $ColorRect.size.x
+			var gap = 10
+			global_position = Vector2(
+				partner_card.global_position.x + card_width + gap,
+				partner_card.global_position.y
+			)
